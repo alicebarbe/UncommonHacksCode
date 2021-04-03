5d10ee7d10d23031d4bc1c6758fe3e91350f99d4
lucienmensah <73512326+LucienMens@users.noreply.github.com>
2021-04-03 15:50:26 -0500

    reddit pulling code

--- hackathon.py
+++ hackathon.py
@@ -17,36 +17,78 @@ reddit = praw.Reddit(client_id="V_XJ0VQSyC9WyQ",      # your client id
                      password = "summerfood26!")     # your reddit password
 #print(reddit.read_only)
 #only image posts
-subreddit = reddit.subreddit('Eyebleach')
-top_subreddit = subreddit.top()
-for submission in subreddit.top(limit=1):
-    print(submission.title, submission.id)
-topics_dict = { "title":[],
+useremotion = input("What emotion do you want? ")
+usersub = {"happy":"wholesomememes+eyebleach+funny",
+           "cute":"aww+kitten",
+           "sad":"sadcringe+meirl",
+           "educational":"todayilearned+explainlikeimfive+worldnews",
+           "neutral":"mildlyinteresting+showerthoughts",
+           "hacker":"ProgrammerHumor+gaming+gadgets",
+           "beautiful":"earthporn+space+InternetIsBeautiful"}
+if useremotion == 'sad':
+    desiredSub = usersub.get('sad')
+elif useremotion == 'happy':
+    desiredSub = usersub.get('happy')
+elif useremotion == 'cute':
+    desiredSub = usersub.get('cute')
+else:
+    print("Your code is broken and you're dumb smh")
+#now i want the length to see how long
+print(len(desiredSub))
+if len(desiredSub) > 0:
+    print("there are multiple subs")
+    #so now i want to run a for loop, adding a number until it reaches the end
+
+#for submission in reddit.subreddit("redditdev+learnpython").top("all"):
+  #  print(submission)    
+subreddit = reddit.subreddit(desiredSub)
+top_subreddit = subreddit.top(limit=300)
+#for submission in subreddit.top(limit=1):
+    #print(submission.title, submission.id)
+def redditpull(subreddit):
+    topics_dict = { "title":[],
                 "score":[],
                 "id":[], "url":[], 
                 "comms_num": [], 
                 "created": [], 
                 "body":[]}
-for submission in top_subreddit:
-    topics_dict["title"].append(submission.title)
-    topics_dict["score"].append(submission.score)
-    topics_dict["id"].append(submission.id)
-    topics_dict["url"].append(submission.url)
-    topics_dict["comms_num"].append(submission.num_comments)
-    topics_dict["created"].append(submission.created)
-    topics_dict["body"].append(submission.selftext)
-    print(top_subreddit.url)
+    for submission in top_subreddit:
+        topics_dict["title"].append(submission.title)
+        topics_dict["score"].append(submission.score)
+        topics_dict["id"].append(submission.id)
+        topics_dict["url"].append(submission.url)
+        topics_dict["comms_num"].append(submission.num_comments)
+        topics_dict["created"].append(submission.created)
+        topics_dict["body"].append(submission.selftext)
     
-topics_data = df(topics_dict)
+    
+    topics_data = df(topics_dict)
  
 #only images specially, or imgur, or image links
 #the url is the most important think
 #urls = df['url']
-print(topics_data.url)
+    print(topics_data.url)
 #i want to feel sad, cute, funny, happy
 
+redditpull(usersub)
+
+'''
+saddness subs:
+    eyebleach
+
+happiness subs:
+wholesomememes
+
+cute things:
+aww
+Kitten
+
+hacker:
+ProgrammerHumor
+    
+irony
+dankmemes
+me_irl
 
 
-#sadness
-#happi
-#https://www.reddit.com/r/wholesomememes/
+'''
