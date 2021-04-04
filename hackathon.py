39f247a3e6da8260d7feb3c1a2275328179b9bae
Pandora Davis <luciendmensah@gmail.com>
2021-04-04 13:20:22 -0500

    work on upvote/downvote feature

--- hackathon.py
+++ hackathon.py
@@ -46,6 +46,13 @@ else:
 
 #the desired sub that i want    
 subreddit = reddit.subreddit(desiredSub)
+print(subreddit.display_name)
+import pprint
+
+# assume you have a Reddit instance bound to variable `reddit`
+#submission = reddit.submission(id="39zje0")
+#print(submission.title)  # to make it non-lazy
+#pprint.pprint(vars(submission))
 #queries 300 posts from the sub
 top_subreddit = subreddit.top(limit=300)
 #this function gives us our information into a neat little dataframe
@@ -65,22 +72,59 @@ def redditpull(subreddit):
         topics_dict["comms_num"].append(submission.num_comments)
         topics_dict["created"].append(submission.created)
         topics_dict["body"].append(submission.selftext)
-        topics_dict["subname"].append(reddit.subreddit(desiredSub))
+        topics_dict["subname"].append(submission.subreddit)
+        global subID
+        subID = topics_dict.get('subname')
         
+      #  substuff = reddit.submission(id=subID)
+       # print(topics_dict.get('subname'))
     topics_data = df(topics_dict)
     #prints the urls of the code
     print(topics_data.url)
+    
     #this randomizes the urls that are given so users see a variety of memes
     global randomize
     randomize = topics_data.url
     random.shuffle(randomize)
-    print(topics_dict.get('subname'))
+   # print(topics_dict.get('subname'))
     return randomize
 
 
 redditpull(usersub)
 #print(randomize)
-print(usersub.get('sad'))
-if '+' in usersub.get('sad'):
-    print("it has that")
+print(type(subID))
+goodmeme = input("Do you like the meme? ")
+memechance = 1/len(set(subID))
+subnames = list(set(subID))
+#print(subnames[0])
+
+sub1 = subnames[0]
+sub2 = subnames[1]
+#look at the subreddit it's from
+if goodmeme == 'yes':
+    #which subreddit is it from
+    sub1chance = memechance + 0.05
+    sub2chance = memechance - 0.05
+elif goodmeme == 'no':
+    sub1chance = memechance - 0.05
+    sub2chance = memechance + 0.05
+if sub1chance > sub2chance:
+    #if subID == sub1:
+        #sub2 delete 5%
+    print(subnames[0], "is a liked sub")
+        #remove 5% of the posts from a certain sub
+        
+    
+    
+
+'''
+https://www.reddit.com/r/flask/comments/chm0qu/have_upvotedownvotes_buttons_that_dont_reload_the/
+do you like the meme? yes?
+5% increase, so it shows more of the memes
+so now considering the length of the distinct values
+of submission subs, i should divide that by one to get 
+the probability
+then if something is selected, it should be rated higher, 
+and the others should decrease
+'''
   #  if topics_data.url in #the subrredit, so which one keeps track of the subreddit
